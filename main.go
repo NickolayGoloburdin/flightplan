8214fdb251b20888e6c3679ef71888e7c21952f9
nickolaygoloburdin <kokakolala1998@gmail.com>
2023-04-04 13:18:49 +0300

    add func to calcDist

--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"image/color"
-	"log"
+	"math"
 	"os"
 
 	"github.com/davvo/mercator"
@@ -43,7 +44,7 @@ func PointtoXY(points []Point) plotter.XYs {
 	}
 	return pts
 }
-func plt(pts plotter.XYs) {
+func plt(pts plotter.XYs) error {
 	p := plot.New()
 	p.Title.Text = "Title"
 	p.X.Label.Text = "X [mm]"
@@ -52,27 +53,36 @@ func plt(pts plotter.XYs) {
 	p.Y.Label.Position = draw.PosTop
 	s, err := plotter.NewScatter(pts)
 	if err != nil {
-		log.Fatal(err.Error())
+		return fmt.Errorf(err.Error())
 	}
 	s.GlyphStyle.Shape = draw.CrossGlyph{}
 	s.Color = color.RGBA{R: 255, A: 255}
 	p.Add(s)
 	wt, err := p.WriterTo(512, 512, "png")
 	if err != nil {
-		log.Fatal("ERROR", err)
+		return fmt.Errorf(err.Error())
 
 	}
 	f, err := os.Create("out.png")
 	defer f.Close()
 	_, err = wt.WriteTo(f)
 	if err != nil {
-		log.Fatal("ERROR", err)
+		return fmt.Errorf(err.Error())
 	}
+	return nil
 }
+
+func Dist(a, b Point) float64 {
+	return math.Sqrt(math.Pow(b.x-a.x, 2) + math.Pow(b.y-a.y, 2))
+}
+
 func main() {
 	points := []Point{Point{45.91043204152349, 50.50752024855144, 0}, Point{45.90929808523507, 50.50737081218062, 0}, Point{45.90721698424876, 50.50676072184594, 0},
 		Point{45.90506986198657, 50.5057890410317, 0}, Point{45.90767218680866, 50.50375778662693, 0}, Point{45.91184648100185, 50.50651414134762, 0}, Point{45.91043204152349, 50.50752024855144, 0}}
 
 	pts := PointtoXY(convertWGStoCart(points))
-
+	err := plt(pts)
+	if err != nil {
+		fmt.Sprintf(err.Error())
+	}
 }
